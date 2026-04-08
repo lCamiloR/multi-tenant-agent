faa7e2822ad2b025f9ec437c0df4b2a944cbc602
Luccas Camilo Roberto <61637025+lCamiloR@users.noreply.github.com>
2026-04-07 22:15:36 -0300

    Implement initial project structure and setup

---
+++ src/agent/graph_agent.py
@@ -0,0 +1,139 @@
+"""Graph agent class."""
+from typing import Callable, Literal
+from langchain.messages import SystemMessage, ToolMessage, HumanMessage
+from langgraph.types import Command
+from langgraph.types import RetryPolicy
+from langchain.tools import tool
+from langchain.chat_models import init_chat_model
+from langgraph.graph import StateGraph, START, END
+
+from src.agent.models import MessagesState
+from langgraph.checkpoint.memory import InMemorySaver
+
+
+class GraphAgent:
+
+    def __init__(self, model: str = "gpt-4o-mini", temperature: float = 0, **kwargs):
+        self.model = init_chat_model(model=model, temperature=temperature, **kwargs)
+        self.agent = self._compile_agent()
+
+    @tool
+    @staticmethod
+    def multiply(a: int, b: int) -> int:
+        """Multiply `a` and `b`.
+
+        Args:
+            a: First int
+            b: Second int
+        """
+        return a * b
+
+
+    @tool
+    @staticmethod
+    def add(a: int, b: int) -> int:
+        """Adds `a` and `b`.
+
+        Args:
+            a: First int
+            b: Second int
+        """
+        return a + b
+
+    @tool
+    @staticmethod
+    def subtract(a: int, b: int) -> int:
+        """Subtracts `a` from `b`.
+
+        Args:
+            a: First int
+            b: Second int
+        """
+        return b - a
+
+    @tool
+    @staticmethod
+    def divide(a: int, b: int) -> float:
+        """Divide `a` and `b`.
+
+        Args:
+            a: First int
+            b: Second int
+        """
+        return a / b
+
+    def llm_call(self, model_with_tools: Callable):
+        """LLM decides whether to call a tool or not"""
+
+        def create_llm_call_node(state: dict):
+            """Create an LLM call node"""
+            return {
+                "messages": [
+                    model_with_tools.invoke(
+                        [
+                            SystemMessage(
+                                content="You are a helpful assistant tasked with performing arithmetic on a set of inputs."
+                            )
+                        ]
+                        + state["messages"]
+                    )
+                ],
+                "llm_calls": state.get('llm_calls', 0) + 1
+            }
+    
+        return create_llm_call_node
+
+    def tool_node(self, tools_by_name: dict):
+        """Performs the tool call"""
+
+        def create_tool_node(state: dict):
+            """Create a tool node"""
+            result = []
+            for tool_call in state["messages"][-1].tool_calls:
+                tool = tools_by_name[tool_call["name"]]
+                observation = tool.invoke(tool_call["args"])
+                result.append(ToolMessage(content=observation, tool_call_id=tool_call["id"]))
+            return {"messages": result}
+        
+        return create_tool_node
+
+    @staticmethod
+    def should_continue(state: MessagesState) -> Literal["tool_node", END]: # type: ignore
+        """Decide if we should continue the loop or stop based upon whether the LLM made a tool call"""
+
+        messages = state["messages"]
+        last_message = messages[-1]
+
+        # If the LLM makes a tool call, then perform an action
+        if last_message.tool_calls: # type: ignore
+            return "tool_node"
+
+        # Otherwise, we stop (reply to the user)
+        return END
+
+    def _compile_agent(self):
+        """Run the graph agent."""
+        # Augment the LLM with tools
+        tools = [self.add, self.subtract, self.multiply, self.divide]
+        tools_by_name = {tool.name: tool for tool in tools}
+        model_with_tools = self.model.bind_tools(tools)
+
+        # Build workflow
+        agent_builder = StateGraph(MessagesState)
+
+        # Add nodes
+        agent_builder.add_node("llm_call", self.llm_call(model_with_tools=model_with_tools)) # type: ignore
+        agent_builder.add_node("tool_node", self.tool_node(tools_by_name=tools_by_name), retry_policy=RetryPolicy(max_attempts=3, initial_interval=1.0)) # type: ignore
+
+        # Add edges to connect nodes
+        agent_builder.add_edge(START, "llm_call")
+        agent_builder.add_conditional_edges(
+            "llm_call",
+            self.should_continue,
+            ["tool_node", END]
+        )
+        agent_builder.add_edge("tool_node", "llm_call")
+
+        # Compile the agent
+        checkpointer = InMemorySaver()
+        return agent_builder.compile(checkpointer=checkpointer)
